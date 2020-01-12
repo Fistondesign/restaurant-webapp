2de15a88f875e2118f579d3d21fcf830133fd674
knfizzo <knfintaganda@gmail.com>
2020-01-12 15:02:13 +0200

    Updates

---
+++ precache-manifest.f7ad7ec1e4db5bd72379a215db2747e7.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "735b3078f0cd079a1f631e0516909445",
+    "url": "/knfizzo/restaurant-webapp.git/index.html"
+  },
+  {
+    "revision": "5890ab513344b61da5ad",
+    "url": "/knfizzo/restaurant-webapp.git/static/css/main.2f0956ea.chunk.css"
+  },
+  {
+    "revision": "6c8be6814677791579e8",
+    "url": "/knfizzo/restaurant-webapp.git/static/js/2.f88f1df4.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/knfizzo/restaurant-webapp.git/static/js/2.f88f1df4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5890ab513344b61da5ad",
+    "url": "/knfizzo/restaurant-webapp.git/static/js/main.18539b15.chunk.js"
+  },
+  {
+    "revision": "faaa5cc903ae612d89cb",
+    "url": "/knfizzo/restaurant-webapp.git/static/js/runtime-main.ab78b81a.js"
+  },
+  {
+    "revision": "dc6d00bb391b2dcbf3e4085912e4b557",
+    "url": "/knfizzo/restaurant-webapp.git/static/media/background.dc6d00bb.png"
+  },
+  {
+    "revision": "dc6d00bb391b2dcbf3e4085912e4b557",
+    "url": "/knfizzo/restaurant-webapp.git/static/media/data.dc6d00bb.png"
+  },
+  {
+    "revision": "5d6e62d821c56e4a18a3c91cbeeba9ba",
+    "url": "/knfizzo/restaurant-webapp.git/static/media/food.5d6e62d8.png"
+  },
+  {
+    "revision": "ec0e6aea6346634cbc251a427b6c3beb",
+    "url": "/knfizzo/restaurant-webapp.git/static/media/phone.ec0e6aea.png"
+  },
+  {
+    "revision": "5099807be95edb5fdf6554c77d45303b",
+    "url": "/knfizzo/restaurant-webapp.git/static/media/resto_bg.5099807b.png"
+  }
+]);
